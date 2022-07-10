04a6c5ba54479e00759e74fa2c593803d65993e6
chibuzodaniel <chibuzodaniel71@gmail.com>
2022-07-10 07:41:08 +0000

    update

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+//adding the cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+//removing the cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+//cart loading something
+
+if (document.readyState === "loading") {
+  document.addEventListener("DomContentLoaded", ready);
+} else {
+  ready();
+}
+
+//creating a function
+function ready() {
+  //removing items from cart
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+}
+//changing quantity
+var quantityInputs = document.getElementsByClassName("cart-quantity");
+for (var i = 0; i < quantityInputs.length; i++) {
+  var inputs = quantityInputs[i];
+  inputs.addEventListener("chage", quantitychanged);
+}
+//removing items from cart
+
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+//quantity changes
+function quantitychanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+//update total
+function updateTotal() {
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+
+    document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  }
+}
